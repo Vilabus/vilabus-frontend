8c2c1f2b389cbd8a0c18cd6afaf69758551ce50e
978sebasBeller <sebastian.beller@ucb.edu.bo>
2025-01-11 21:51:30 -0400

    The main text from the Home Page changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1>Sila-bus.com is under construction</h1>
+      <h1>Sila-bus.com is under construction 🙂</h1>
     </div>
   );
 }
